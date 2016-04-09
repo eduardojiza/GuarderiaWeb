5b48346a1211a32393c95a0f27ca8a0753f58a01
Eduardo Jimenez <eduardo@MacBook-Pro-de-Eduardo.local>
2016-04-09 09:02:11 -0500

    Inicio de proyecto

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name guarderiaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the guarderiaApp
+ */
+angular.module('guarderiaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
